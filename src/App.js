34e693598e0934ae7c52b90a2d532faeb70b2a9a
Victor Lu <victlu@microsoft.com>
2024-08-31 10:39:19 -0700

    initial version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,140 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [getCount, setCount] = React.useState(0);
+  const [getFilter, setFilter] = React.useState(null);
+  const [getSelect, setSelect] = React.useState(null);
+
+  const clickAddGroup = (e, i) => {
+    let filter = getFilter;
+    let obj = [ { field: "new", op: "=", value: "new-" + getCount } ];
+    filter.splice(i+1, 0, obj);
+    setFilter(filter);
+    setCount(getCount+1);
+  }
+
+  const clickRemoveGroup = (e, i) => {
+    let filter = getFilter;
+    filter.splice(i, 1);
+    setFilter(filter);
+    setCount(getCount+1);
+  }
+
+  const clickAddTerm = (e, i) => {
+    let filter = getFilter;
+    filter[i].push({ field: "new", op: "=", value: "new"});
+    setFilter(filter);
+    setCount(getCount+1);
+  }
+
+  const clickEditItem = (e, i, j) => {
+    if (getSelect && getSelect.row === i && getSelect.term === j)
+    {
+      setSelect(null);
+    }
+    else
+    {
+      setSelect({ row: i, term: j});
+    }
+  }
+
+  let filter = getFilter;
+  if (!filter)
+  {
+    filter = [
+      [
+        { field: "hello1", op: "=", value: "that1" },
+        { field: "hello2", op: "=", value: "that2" },
+        { field: "hello3", op: "=", value: "that2" },
+      ],
+      [
+        { field: "hello3", op: "=", value: "that3" },
+        { field: "hello4", op: "=", value: "that4" },
+      ]
+    ];
+    setFilter(filter);
+  }
+
+  let body = [];
+  for (let i = 0; i < filter.length; i++)
+  {
+    let fields = [];
+    for (let j = 0; j < filter[i].length; j++)
+    {
+      let rec = filter[i][j];
+
+      let cond = <span
+        className="badge bg-primary cursor-clickable mr-1"
+        onClick={ e => { clickEditItem(e, i, j)}}
+        >{rec.field} {rec.op} {rec.value}</span>
+
+      if (getSelect && getSelect.row === i && getSelect.term === j)
+      {
+        cond = <span
+          className="badge bg-success mr-1">
+            <div>{rec.field}</div>
+            <div>{rec.op}</div>
+            <div>{rec.value}</div>
+            <div>
+              <span
+              className="cursor-clickable"
+              onClick={ e => { clickEditItem(e, i, j)}}
+              >Close</span>
+            </div>
+          </span>
+      }
+
+      if (j > 0)
+      {
+        fields.push(<span className="cursor-normal"> AND </span>);
+      }
+      fields.push(cond);
+    }
+
+    fields.push(<span 
+      className="badge bg-success cursor-clickable mr-1"
+      onClick={ e => { clickAddTerm(e, i);}}
+      >+</span>);
+
+    if (i > 0)
+    {
+      body.push(<div>OR</div>)
+    }
+    body.push(<div>
+      <span className="badge bg-secondary mr-1">{fields}</span>
+      <span 
+        className="badge bg-success cursor-clickable mr-1"
+        onClick={ e => { clickAddGroup(e, i);}}
+        >+</span>
+      <span 
+        className="badge bg-success cursor-clickable"
+        onClick={ e => { clickRemoveGroup(e, i);}}
+        >-</span>
+      </div>);
+  }
+
+  if (filter.length == 0)
+  {
+    body.push(
+      <div>
+      <span 
+      className="badge bg-success cursor-clickable mr-1"
+      onClick={ e => { clickAddGroup(e, 0);}}
+      >+</span>
+      </div>
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Edit AST</p>
       </header>
+      <body className="App-body">
+        <div>Filter:</div>
+        {body}
+      </body>
     </div>
   );
 }
